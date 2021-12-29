2312da4e681aa6eedfaec85157384fb6cea0026a
BDz3us <lucas.emanuel@indwise.com>
2021-12-29 15:45:18 -0300

    navigation implementation

---
+++ android/app/src/pages/login/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Button } from 'react-native';
+const Login = ({ navigation }) => {
+    return (
+      <Button
+        title="Tela de login"
+
+      />
+    );
+  };
+  export default Login;
