fb455224a0f0552b10e1a24fc709de8a84d00dbf
LucasDevSystem <highspeedsetup@gmail.com>
2021-12-31 11:30:18 -0300

    card simple implementation

---
+++ android/app/src/pages/main/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import {TextInput, Button, Text} from 'react-native-paper';
+import Card from './card';
+
+const MainPage = ({navigation}) => {
+  return (
+    <View>
+      <Card />
+    </View>
+  );
+};
+
+export default MainPage;
